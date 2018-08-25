d4bda9649084260548aace5d13f87eaa7778f4e4
Vincenzo <vincenzo.consales95@gmail.com>
2018-08-25 19:03:54 +0200

    added ggsave

--- RRcellNet/simulations/analyze_csv.r
+++ RRcellNet/simulations/analyze_csv.r
@@ -10,6 +10,7 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
 	plots <- c(list(...), plotlist)
 	numPlots = length(plots)
 
+	ggsave("plot.png", dpi=320)
 	if (is.null(layout)) {
 		layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
 		ncol = cols, nrow = ceiling(numPlots/cols))
@@ -266,6 +267,7 @@ plotModuleComparision <- function(plotdata1, moduleindex1, plotdata2, moduleinde
 	plot_th <- ggplot(plotdata, aes(x=usertraffic, y=throughput.mean, colour=interaction(module, scenario), group=interaction(module, scenario))) +
 		geom_line() + scale_y_continuous(breaks=seq(0,y_max,y_max/32)) + scale_x_continuous(breaks=seq(0,x_max,1)) +
 		geom_errorbar(aes(ymin=throughput.confmin, ymax=throughput.confmax, width=.1))
+	ggsave("plotth.png", dpi=320)	
 
 	y_max <- round(max(plotdata$responsetime.mean)/(1), digits=0)	
 	plot_rt <- ggplot(plotdata, aes(x=usertraffic, y=responsetime.mean, colour=interaction(module, scenario), group=interaction(module, scenario))) +
@@ -273,6 +275,8 @@ plotModuleComparision <- function(plotdata1, moduleindex1, plotdata2, moduleinde
 		geom_errorbar(aes(ymin=responsetime.confmin, ymax=responsetime.confmax, width=.1))
 
 	plotdouble_singlelegend(plot_th, plot_rt)
+
+	ggsave("plotresp.png", dpi=320)
 }
 
 plotModuleRBComparision <- function(plotdata1, moduleindex1, plotdata2, moduleindex2) {
