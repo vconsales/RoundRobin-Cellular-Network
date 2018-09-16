51eba99d540ab1ae3639a9e18d173d4a126504b1
Antonio le Caldare <antonio.lecaldare@gmail.com>
2018-09-16 20:24:52 +0200

    updated rbatch_images with missing images, updated missing images in list

--- RRcellNet/simulations/rbatch_images.sh
+++ RRcellNet/simulations/rbatch_images.sh
@@ -3,29 +3,49 @@
 # batch generate all the images
 r analyze_csv.r <<EOF
 mode png
-all binom
-close
 all binombest
 close
-ecdf unif unifbest 2.1 0
+all binom
 close
-ecdf unif unifbest 3.1 0
+all nofram
 close
-lorallrt unifbest
+allrbbars binom binombest 4.1
 close
-lorallth unifbest
+allrbbars binom binombest 9.1
+close
+allrbbars unif unifbest 1.1
+close
+all unifbest
+close
+all unif
 close
 all val1
 close
 all val2
 close
+ecdf unif unifbest 2.1 0
+close
+ecdf unif unifbest 3.1 0
+close
+fillrb nofram
+close
+lorallrb binombest
+close
+lorallrt unifbest
+close
+lorallth binombest
+close
+lorallth unifbest
+close
 rb val2 val2 0 1
 close
 thantenna 1
 close
-all unif
+thantenna 2
 close
-all unifbest
+thantennamax 1
+close
+th binom binombest 5 5
 close
 exit
 EOF
