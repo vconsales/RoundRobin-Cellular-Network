11877a95e9011d4cd41540856e3c8b7ba62cc34a
Antonio le Caldare <antonio.lecaldare@gmail.com>
2018-09-20 01:16:23 +0200

    scheduler: use ceiling instead of round. BIG BUG

--- RRcellNet/src/Scheduler.cc
+++ RRcellNet/src/Scheduler.cc
@@ -54,7 +54,8 @@ void Scheduler::initialize()
 
 int integerRoundDivision(const int n, const int d)
 {
-    return ((n < 0) ^ (d < 0)) ? ((n - d/2)/d) : ((n + d/2)/d);
+    //return ((n < 0) ^ (d < 0)) ? ((n - d/2)/d) : ((n + d/2)/d);
+    return (n + d - 1) / d;
 }
 
 void Scheduler::handleMessage(cMessage *msg)
